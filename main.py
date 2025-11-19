a021b857087d784f7475b47f42ecc97d0fec4e14
amanda16229 <amanda.olyer@gmail.com>
2025-11-18 21:00:39 -0500

    made new file for ascii art and added some print statements for testing

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # main
 
+import simulated_bomb, image_scanner, timer_ascii_art
+
+print("Main module running")
+
+print(timer_ascii_art.logo)
+
+simulated_bomb.bomb_status()
+
+
--- simulated_bomb.py
+++ simulated_bomb.py
@@ -1,1 +1,21 @@
 # code for simulated_bomb
+
+import time
+import timer_ascii_art
+
+# add timer that calls this function every few seconds, can make this a list and access it using indexes where each index is shows ascii art of bomb ticking down
+# can make a new file to only include bomb ascii art and timer- define it as a list and import it here
+
+
+def bomb_status():
+    print("Checking bomb status...")
+    time.sleep(2)
+    print(timer_ascii_art.bomb)
+    print("Bomb is active.")
+    time.sleep(2)
+    print("Bomb will detonate in 10 seconds!")
+    time.sleep(10)
+    print(timer_ascii_art.explosion)
+    print("BOOM! The bomb has detonated.")
+    return 0
+
