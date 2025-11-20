abd7ddbdd21b1f9758ab18af47b547b3b6958472
amanda16229 <amanda.olyer@gmail.com>
2025-11-20 09:51:20 -0500

    import tkinter added notes for later

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 
 import simulated_bomb, image_scanner, timer_ascii_art
 
+import tkinter as tk
+
+# run tkinter window in main, call other modules from here
+# will need to import images to display in tkinter window
+# have ascii cli art still in case tkinter doesnt work how we want
+
 print("Main module running")
 
 print(timer_ascii_art.logo)
